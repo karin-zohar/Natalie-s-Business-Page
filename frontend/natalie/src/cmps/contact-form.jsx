35ea3c5926b200a82476e7278180acecb6ee04ea
Karin Zohar <karinzohar3@gmail.com>
2024-02-04 19:19:18 +0200

    designed basic form layout

--- frontend/natalie/src/cmps/contact-form.jsx
+++ frontend/natalie/src/cmps/contact-form.jsx
@@ -1,5 +1,5 @@
 
-import {useState} from 'react'
+import { useState } from 'react'
 import { Formik, Form, Field, getIn } from 'formik'
 import * as Yup from 'yup'
 
@@ -12,7 +12,7 @@ export function ContactForm() {
     }
     const [contactRecord, setContactRecord] = useState(emptyContactRecord)
     const [isSubmitted, setIsSubmitted] = useState(false)
-    
+
     async function onSubmit(contactRecord) {
         try {
             console.log('contactRecord: ', contactRecord)
@@ -23,10 +23,36 @@ export function ContactForm() {
     }
 
     return (
-        <section className="contact-form">
-        <h2>צרו קשר</h2>
-        <article className="form-container">
-        </article>
+        <section className="contact-form main-layout full">
+            <h2>צרו קשר</h2>
+            <article className="form-container">
+                <Formik
+                    initialValues={contactRecord}
+                    onSubmit={onSubmit}
+                    enableReinitialize={true}
+                //TODO: add validation schema
+                >
+                    {({ errors, touched }) => (
+                        <Form>
+                            <label htmlFor='name'>
+                                <p>שם</p>
+                                <Field type="name" name="name" placeholder="שם" />
+                            </label>
+                            <label htmlFor='email'>
+                                <p>אימייל</p>
+                                <Field type="email" name="email" placeholder="אימייל" />
+                            </label>
+                            <label htmlFor='phoneNumber'>
+                                <p>טלפון</p>
+                                <Field type="phoneNumber" name="phoneNumber" placeholder="טלפון" />
+                            </label>
+                            <button className="submit-btn" type="submit">שלח</button>
+                        </Form>
+                    )}
+
+                </Formik>
+
+            </article>
         </section>
     )
 }
