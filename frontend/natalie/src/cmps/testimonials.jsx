f95ab080946ffc18d5b2b5b8b2834e3c4e6bacc8
Karin Zohar <karinzohar3@gmail.com>
2024-05-16 12:51:24 +0300

    Fixed testimonials carousel's display.

--- frontend/natalie/src/cmps/testimonials.jsx
+++ frontend/natalie/src/cmps/testimonials.jsx
@@ -1,10 +1,13 @@
 import SlCarousel from '@shoelace-style/shoelace/dist/react/carousel'
 import SlCarouselItem from '@shoelace-style/shoelace/dist/react/carousel-item'
 import { testimonials } from '../constants/hompage-constants'
+import { useState } from 'react'
 
 export function Testimonials() {
   console.log('testimonials: ', testimonials)
   const slidesPerPage = 1
+
+
   return (
     <>
 
@@ -19,11 +22,13 @@ export function Testimonials() {
         slidesPerMove={1}
 
         className="carousel scroll-hint"
-        style={{ '--scroll-hint': '10%' }}
+        style={{
+          '--scroll-hint': '10%',
+        }}
       >
         {testimonials.map((tes, idx) => {
           return (
-            <SlCarouselItem key={idx} >
+            <SlCarouselItem key={idx} className='carousel-item'>
               <article className="carousel-item-container">
                 <img
                   alt={tes.videoAltTest}
@@ -35,7 +40,7 @@ export function Testimonials() {
           )
 
         })}
-      </SlCarousel>
+      </SlCarousel >
     </>
 
   )
